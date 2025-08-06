25eab2f5786dec5a065f47331008ab18e945e538
Fred120-code <fredayemtsa@gmail.com>
2025-08-06 15:39:05 +0100

    quelques modifications

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
-const API = axios.create( {
-    baseURL:'http://localhost:6000/api',
+const API = axios.create({
+    baseURL:'http://localhost:6000/api/auth',
     withCredentials: true
 })
 
 export const registerUser = (data:{email:string; password:string; name:string}) =>{
     return API.post('/register', data)
 }
+
+//
